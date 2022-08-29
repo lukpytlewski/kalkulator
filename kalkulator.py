49115cb96929eafcab90c62c1d2139036dac8c1d
Lukasz Pytlewski <lukpytlewski@yahoo.com>
2022-08-29 23:08:47 +0200

    Pierwsza wersja rozwiązania

---
+++ kalkulator.py
@@ -0,0 +1,37 @@
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+dzialanie=input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: ")
+if dzialanie=="1":
+    liczba1=input("Podaj składnik 1. ")
+    liczba2=input("Podaj składnik 2. ")
+    logging.info("Dodaję %.2f i %.2f." %(float(liczba1), float(liczba2)))
+    wynik = float(liczba1) + float(liczba2)
+
+elif dzialanie=="2":
+    liczba1=input("Podaj odjemną. ")
+    liczba2=input("Podaj odjemnik. ")
+    logging.info("Od %.2f odejmuję %.2f." %(float(liczba1), float(liczba2)))
+    wynik = float(liczba1) - float(liczba2)
+
+elif dzialanie=="3":
+    liczba1=input("Podaj czynnik 1. ")
+    liczba2=input("Podaj czynnik 2. ")
+    logging.info("Mnożę %.2f i %.2f." %(float(liczba1), float(liczba2)))
+    wynik = float(liczba1) * float(liczba2)
+
+elif dzialanie=="4":
+    liczba1=input("Podaj dzielną. ")
+    liczba2=input("Podaj dzielnik. ")
+    if liczba2=="0":
+        wynik="Kto dzieli przez 0, ten się w piekle poniewiera."
+    elif liczba2!=0:
+        logging.info("Dzielę %.2f przez %.2f." %(float(liczba1), float(liczba2)))
+        wynik = float(liczba1) / float(liczba2)
+
+if dzialanie=="4" and liczba2=="0":
+    print(wynik)
+elif dzialanie!="1" and dzialanie!="2" and dzialanie!="3" and dzialanie!="4":
+    print("Przykro mi - nie wykonałeś polecenia wg instrukcji.")
+else:
+    print("Wynik to %.2f" %(wynik))
