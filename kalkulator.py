7292f741d4bc90e4d92f7f12f6f10813c9d401f4
Lukasz Pytlewski <lukpytlewski@yahoo.com>
2022-08-30 18:25:22 +0200

    Trzeca wersja rozwiązania

--- kalkulator.py
+++ kalkulator.py
@@ -1,41 +1,74 @@
 import logging
+
 logging.basicConfig(level=logging.DEBUG)
 
-dzialanie=input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: ")
-if dzialanie=="1":
-    liczba_skladnikow=input("Podaj liczbę składników: ")
-    skladniki=[]
-    wynik=0
-    for i in range(0, int(liczba_skladnikow)):
-        skladnik=input("Podaj kolejny składnik. ")
-        skladniki.append(skladnik)
-        wynik=wynik+float(skladnik)
-    logging.info("Dodaję %.0f składniki(ów)" %(len(skladniki)))
 
-elif dzialanie=="2":
-    liczba1=input("Podaj odjemną. ")
-    liczba2=input("Podaj odjemnik. ")
-    logging.info("Od %.2f odejmuję %.2f." %(float(liczba1), float(liczba2)))
-    wynik = float(liczba1) - float(liczba2)
+def dodawanie(liczba1, liczba2):
+    return liczba1 + liczba2
 
-elif dzialanie=="3":
-    liczba1=input("Podaj czynnik 1. ")
-    liczba2=input("Podaj czynnik 2. ")
-    logging.info("Mnożę %.2f i %.2f." %(float(liczba1), float(liczba2)))
-    wynik = float(liczba1) * float(liczba2)
 
-elif dzialanie=="4":
-    liczba1=input("Podaj dzielną. ")
-    liczba2=input("Podaj dzielnik. ")
-    if liczba2=="0":
-        wynik="Kto dzieli przez 0, ten się w piekle poniewiera."
-    elif liczba2!=0:
-        logging.info("Dzielę %.2f przez %.2f." %(float(liczba1), float(liczba2)))
-        wynik = float(liczba1) / float(liczba2)
+def odejmowanie(liczba1, liczba2):
+    return liczba1 - liczba2
 
-if dzialanie=="4" and liczba2=="0":
-    print(wynik)
-elif dzialanie!="1" and dzialanie!="2" and dzialanie!="3" and dzialanie!="4":
-    print("Przykro mi - nie wykonałeś polecenia wg instrukcji.")
-else:
-    print("Wynik to %.2f" %(wynik))
+
+def mnozenie(liczba1, liczba2):
+    return liczba1 * liczba2
+
+
+def dzielenie(liczba1, liczba2):
+    if liczba2 == 0:
+        return "Kto dzieli przez 0, ten się w piekle poniewiera"
+    else:
+        return liczba1 / liczba2
+
+
+if __name__ == "__main__":
+    dzialanie = input(
+        "Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: "
+    )
+    if dzialanie not in ["1", "2", "3", "4"]:
+        print("Przykro mi, nie wykonałeś polecenia poprawnie.")
+    else:
+
+        if dzialanie == "1":
+            liczba_skladnikow = input("Podaj liczbę składników: ")
+            skladniki = []
+            wynik = 0
+            for i in range(int(liczba_skladnikow)):
+                skladnik = float(input("Podaj kolejny składnik. "))
+                skladniki.append(skladnik)
+                if i == 0:
+                    pass
+                else:
+                    logging.info("Dodaję %.2f i %.2f" % (wynik, skladnik))
+                wynik = dodawanie(wynik, skladniki[i])
+
+        elif dzialanie == "2":
+            liczba1 = float(input("Podaj odjemną. "))
+            liczba2 = float(input("Podaj odjemnik. "))
+            logging.info("Od %.2f odejmuję %.2f." % (liczba1, liczba2))
+            wynik = odejmowanie(liczba1, liczba2)
+
+        elif dzialanie == "3":
+            liczba_czynnikow = float(input("Podaj liczbę czynników: "))
+            czynniki = []
+            wynik = 1
+            for i in range(int(liczba_czynnikow)):
+                czynnik = float(input("Podaj kolejny czynnik. "))
+                czynniki.append(czynnik)
+                if i == 0:
+                    pass
+                else:
+                    logging.info("Mnożę %.2f i %.2f." % (wynik, czynnik))
+                wynik = mnozenie(wynik, czynniki[i])
+
+        elif dzialanie == "4":
+            liczba1 = float(input("Podaj dzielną. "))
+            liczba2 = float(input("Podaj dzielnik. "))
+            logging.info("Dzielę %.2f przez %.2f." % (liczba1, liczba2))
+            wynik = dzielenie(liczba1, liczba2)
+
+        if dzialanie == "4" and liczba2 == 0:
+            print(wynik)
+        else:
+            print("Wynik to: %.2f" % (wynik))
